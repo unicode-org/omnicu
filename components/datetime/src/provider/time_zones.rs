b6df5ad35abf5d29ec8470a89cf542b6b8c160d7
Greg Tatum <tatum.creative@gmail.com>
2021-05-17 17:01:03 -0500

    Add various pieces of documentation

--- components/datetime/src/provider/time_zones.rs
+++ components/datetime/src/provider/time_zones.rs
@@ -10,6 +10,7 @@ use std::borrow::Cow;
 macro_rules! map_access {
     ($outer: ty => $inner: ty: $lt: lifetime) => {
         impl<$lt> $outer {
+            /// Get the data from the key.
             pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&$inner>
             where
                 Q: Ord,
@@ -18,6 +19,7 @@ macro_rules! map_access {
                 self.0.get(key)
             }
 
+            /// Check if the underlying data is empty.
             pub fn is_empty(&self) -> bool {
                 self.0.is_empty()
             }
